eb3fa45ab07785d71e24b2742be7d61ceb667550
jav1prl <jav1prl@gitlab.com>
2025-05-26 16:40:08 -0500

    * fix nginx proxy and dockerfile

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,35 +1,33 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { CustomersModule } from './customers/customers.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { DeliveriesModule } from './deliveries/deliveries.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { PaymentsModule } from './payment/payments.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USERNAME'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_DATABASE'),
-        entities: ['dist/**/*.entity.js'],
+        url: config.get<string>('DATABASE_URL'),
         synchronize: config.get<boolean>('DB_SYNCHRONIZE'),
+        entities: ['dist/**/*.entity.js'],
       }),
     }),
+
     ProductsModule,
     CustomersModule,
     TransactionsModule,
     DeliveriesModule,
-    PaymentsModule
+    PaymentsModule,
   ],
 })
 export class AppModule {}
